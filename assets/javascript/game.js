f8b66542ec83bdfba414164f5ec1fe24dc19a3eb
Kona Tess <konatess@gmail.com>
2018-08-19 23:43:34 -0700

    Pseudo coded, with basic layout in HTML.

---
+++ assets/javascript/game.js
@@ -0,0 +1,36 @@
+// need arrays for
+// words 
+    var words = ["Nasal", "Lacrimal", "Maxiallary", "Zygomatic", "Temporal", "Palatine", "Parietal", "Malleus", "Incus", "Stapes", "Frontal", "Ethmoid", "Vomer", "Sphenoid", "Mandible", "Occipital", "Ribs", "Hyoid", "Sternum", "Cervical Vertebrae", "Thoracic Vertebrae", "Lumbar Vertebrae", "Sacrum", "Coccyx", "Scapula", "Clavicle", "Humerus", "Radius", "Ulna", "Scaphoid", "Lunate", "Triquetrum", "Pisiform", "Hamate", "Capitate", "Trapezoid", "Trapezium", "Metacarpal", "Phalanges", "Ilium ", "Ischium", "PubisFemur", "Patella", "Tibia", "Fibula", "Talus", "Calcaneus", "Navicular", "Cuneiforms", "Cuboid"]
+// winning gifs 
+    var winGif = ['<iframe src="https://giphy.com/embed/OUp72ZhJAtsJO" width="480" height="283" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="https://giphy.com/embed/hk3W7hxkLDkA0" width="480" height="204" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="https://giphy.com/embed/14na7KVDve6rHq" width="480" height="357" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="https://giphy.com/embed/jNSooJdHt3Sta" width="480" height="336" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="https://giphy.com/embed/3oriNU7UXUPX91SvNC" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="https://giphy.com/embed/3oEjI53nBYOOEQgDcY" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="https://giphy.com/embed/r6aZOSxM0hOuY" width="480" height="361" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="https://giphy.com/embed/8FUNh9xgfc8HC" width="480" height="361" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>']
+// losing gifs 
+    var loseGif = ['<iframe src="https://giphy.com/embed/qUHxAava8vmUg" width="480" height="288" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="https://giphy.com/embed/XcL6a7YHm4sWA" width="480" height="368" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="https://giphy.com/embed/NrZd0l96TbRcI" width="480" height="240" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="https://giphy.com/embed/35qf6Nu98jJDi" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="https://giphy.com/embed/qKmwOvs6XuRYA" width="480" height="227" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="https://giphy.com/embed/441veZw9vyx56" width="480" height="257" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="https://giphy.com/embed/jpbAaUG7cjkZy" width="480" height="351" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="https://giphy.com/embed/xTkcELnWz4YdJvszfy" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>']
+// starting message
+
+
+
+// play code
+    // select word
+
+    // display blank spaces
+
+    // start with wins = 0, loss = 0
+
+        // take guess input
+        // is guess correct?
+            // if guess correct, display letter(s) in correct location
+            // if guess not correct, append to guessed list
+
+        // if all letters filled in, win
+        // else if all guess length = 15, lose
+        // else, keep playing
+    // if win 
+        // wins = wins + 1
+        // show a winning gif
+        // play winning sound
+    // else lose
+        // lose = lose + 1
+        // show a losing gif
+        // play losing sound
+
+// start over
